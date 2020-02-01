e03a9e4829f74c5d6916d9eaa54bba0d0c3d8c86
ryy <2524380603@qq.com>
2020-02-01 22:14:12 +0800

    added a ex2.cpp file

---
+++ lab2/ex2/ex2.cpp
@@ -0,0 +1,26 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+	typedef int ryy;
+	ryy a = 1, b ,c,d;
+	cout << "triangle\n";
+	cout << "please input how large the triangle is:";
+		cin >> c;
+		for (a = 1; a <= c; a++) {
+			for (d = 1; d <= c - a; d++) {
+				cout << " ";
+			}
+				for (b = 1; b <= a; b++) {
+					
+					cout<<" " << c;
+			
+				}
+				cout << "\n";
+			
+		}
+
+	system("pause");
+}
+
