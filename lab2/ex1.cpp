c14b10c93cf1e169a2ae4432257d2d7209d0b969
ryy <2524380603@qq.com>
2020-02-01 21:32:27 +0800

    added a ex1.cpp file

---
+++ lab2/ex1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "square\n";
+	cout << "please input the length of the square:";
+	int i = 1, m;
+	cin >> m;
+	if (m < 1)
+		cout << "illegal input";
+	if (m >= 80)
+		cout << "the square is too big,please input a small number.";
+	if (m >= 1 && m < 80)
+		for (i = 1; i <= m * m; i++)
+			if (i % m != 0)
+				cout << "*";
+			else
+				cout << "*" << "\n";
+  system("pause");
+}
+
