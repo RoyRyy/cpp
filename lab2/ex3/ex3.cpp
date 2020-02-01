20d8781d98cf59fa5d7cfde7bf3aa0f18452858c
ryy <2524380603@qq.com>
2020-02-01 22:27:10 +0800

    added a ex3.cpp file

---
+++ lab2/ex3/ex3.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	typedef int ryy;
+	ryy a = 1, b, c, d;
+	cout << "diamond\n";
+	cout << "please input how large the diamond is:";
+	cin >> c;
+	for (a = 1; a <= c; a++) {
+		cout << "   ";
+		for (d = 1; d <= c - a; d++) {
+			cout << " ";
+		}
+		for (b = 1; b <= a; b++) {
+
+			cout << " " << c;
+
+		}
+		cout << "\n";
+
+	}
+	for (a = c-1; a >= 1; a--) {
+		cout << "   ";
+		for (d = c - a; d >= 1; d--) {
+			cout << " ";
+		}
+		for (b = a; b >= 1; b--){
+			cout << " " << c;
+		}
+		cout << "\n";
+	}
+
+
+	system("pause");
+}
