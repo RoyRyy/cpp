95fcb7d0c5e87d4f57fd8e39213bc8cc5e17eca7
ryy <2524380603@qq.com>
2020-02-06 11:26:20 +0800

    added a 统计1的位数.file

---
+++ lab4/ex1/ex1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	typedef int ryy;
+	ryy a, b=1, c,count=0;
+	cout << "please input a number:";
+	cin >> a;
+	while (b != 0) {
+		c = a & b;
+		b = b << 1;
+		if (c != 0)
+			count++;
+	}
+	cout << count;
+	cout << "\n";
+	system("pause");
+}
+
