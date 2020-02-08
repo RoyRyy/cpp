376de77acfec52846ea076716f3d584576537077
ryy <2524380603@qq.com>
2020-02-08 10:38:06 +0800

    函数递归调用

---
+++ lab5/ex1/ex1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int Fac(int n)
+{
+	if (n == 0)
+		return 1;
+	else
+		return n * Fac(n - 1);
+}
+int main()
+{
+	int n, m;
+	cout << "Please input a number:";
+	cin >> n;
+	m = Fac(n);
+	cout <<n<<"!="<< m;
+	cout << "\n";
+	system("pause");
+
+}
+
