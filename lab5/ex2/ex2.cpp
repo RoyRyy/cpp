53c18280f685dd54a6fb9d439cff42e7d32bc703
ryy <2524380603@qq.com>
2020-02-10 13:20:26 +0800

    added a Max function file

---
+++ lab5/ex2/ex2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+double Max(double a, double b)
+{
+	if (a >= b)
+		return a;
+	else
+		return b;
+}
+int main()
+{
+	double m, n,k;
+	cout << "Please input two numbers:";
+	cin >> m>>n;
+	k = Max(m, n);
+	cout << "The max is:" << k;
+	cout << "\n";
+	system("pause");
+}
