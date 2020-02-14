550592c386113eaa69178abbb909c95b1892e07c
ryy <2524380603@qq.com>
2020-02-14 15:19:00 +0800

    added a ex2.cpp file

---
+++ lab3/ex2/ex2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	char a[100], b[50];
+	int i=0, j=0;
+	cout << "Please input string 1:" << endl;
+	cin >> a;
+	cout << "Please input string 2:"<< endl;
+	cin >> b;
+	while (a[i] != '\0')
+		i++;
+	while (b[j] != '\0') {
+		a[i++] = b[j++];
+		a[i] = '\0';
+	}
+	cout << a << endl;
+	system("pause");
+	    
+
+
+
+}
+
